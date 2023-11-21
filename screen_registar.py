fb6752a87fc56f585e7efc8707464afed9cffef4
jmbhcabral <larbak.cstrike@gmail.com>
2023-11-21 23:13:04 +0000

    App Dev - kivymd screen_registar.kv layout

--- screen_registar.py
+++ screen_registar.py
@@ -1,5 +1,17 @@
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.relativelayout import MDRelativeLayout
+from kivy.properties import StringProperty
 
 
 class RegistarScreen(Screen):
     pass
+
+
+class ClickableTextFieldRound(MDRelativeLayout):
+    text = StringProperty()
+    hint_text = StringProperty()
+
+
+class ClickableTextFieldRound2(MDRelativeLayout):
+    text = StringProperty()
+    hint_text = StringProperty()
