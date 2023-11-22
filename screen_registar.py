ef68481a1fa23c6442eabe339b855709b49918c3
jmbhcabral <larbak.cstrike@gmail.com>
2023-11-22 23:01:14 +0000

    App Dev - kivymd screen_registar user data

--- screen_registar.py
+++ screen_registar.py
@@ -3,24 +3,40 @@ from kivymd.uix.relativelayout import MDRelativeLayout
 from kivy.properties import StringProperty
 from kivymd.uix.pickers import MDDatePicker
 from kivymd.uix.menu import MDDropdownMenu
+import requests
 
 
 class RegistarScreen(Screen):
-    def on_kv_post(self, base_widget):
-        menu_items = [
+    def on_enter(self, *args):
+        self.ids.nome_label.text = ""
+        self.ids.apelido_label.text = ""
+        self.ids.username_label.text = ""
+        self.ids.email_label.text = ""
+        self.ids.password_round.ids.password_label.text = ""
+        self.ids.password2_round.ids.password2_label.text = ""
+        self.ids.telemovel_label.text = ""
+        self.ids.clickable_date.ids.data_nascimento_label.text = ""
+        self.ids.drop_item.text = ""
+
+    def menu_list(self):
+        self.menu_items = [
             {
+                "viewclass": "OneLineListItem",
                 "text": "Sim, na Esc. Sec. Ramada",
                 "on_release": lambda x="Sim, na Esc. Sec. Ramada": self.set_item(x),
             },
             {
+                "viewclass": "OneLineListItem",
                 "text": "Sim no Agrup. Vasco Santana",
                 "on_release": lambda x="Sim no Agrup. Vasco Santana": self.set_item(x),
             },
             {
+                "viewclass": "OneLineListItem",
                 "text": "Sim noutra escola",
                 "on_release": lambda x="Sim noutra escola": self.set_item(x),
             },
             {
+                "viewclass": "OneLineListItem",
                 "text": "Não",
                 "on_release": lambda x="Não": self.set_item(x),
             },
@@ -28,17 +44,56 @@ class RegistarScreen(Screen):
         ]
         self.menu = MDDropdownMenu(
             caller=self.ids.drop_item,
-            items=menu_items,
+            items=self.menu_items,
             position="center",
             width_mult=4,
         )
 
-    def open_dropdown(self):
         self.menu.open()
 
     def set_item(self, text_item):
-        self.ids.drop_item.set_item(text_item)
+        self.ids.drop_item.text = text_item
         self.menu.dismiss()
+
+    def register_user(self):
+        nome = self.ids.nome_label.text
+        apelido = self.ids.apelido_label.text
+        username = self.ids.username_label.text
+        email = self.ids.email_label.text
+        password = self.ids.password_round.ids.password_label.text
+        password2 = self.ids.password2_round.ids.password2_label.text
+        telemovel = self.ids.telemovel_label.text
+        data_nascimento = self.ids.clickable_date.ids.data_nascimento_label.text
+        escola = self.ids.drop_item.text
+
+        print('Nome: ', nome)
+        print('Apelido: ', apelido)
+        print('Username: ', username)
+        print('Email: ', email)
+        print('Password: ', password)
+        print('Password2: ', password2)
+        print('Telemovel: ', telemovel)
+        print('Data de Nascimento: ', data_nascimento)
+        print('Escola: ', escola)
+
+        data = {
+            'nome': nome,
+            'apelido': apelido,
+            'username': username,
+            'email': email,
+            'password': password,
+            'password2': password2,
+            'telemovel': telemovel,
+            'data_nascimento': data_nascimento,
+            'escola': escola
+        }
+
+        response = requests.post(
+            'http://127.0.0.1:8000/users/api/v1', json=data)
+
+        if response.status_code == 201:
+            print('Utilizador registado com sucesso')
+            self.manager.current = 'screen_login'
 
 
 class ClickableTextFieldRound(MDRelativeLayout):
