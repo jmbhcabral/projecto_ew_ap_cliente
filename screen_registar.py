836d74fa1cad82a3ed9d3b6c754eaded080609c9
jmbhcabral <larbak.cstrike@gmail.com>
2023-11-24 11:09:27 +0000

    App development - register validations

--- screen_registar.py
+++ screen_registar.py
@@ -1,6 +1,6 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.relativelayout import MDRelativeLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivymd.uix.pickers import MDDatePicker
 from kivymd.uix.menu import MDDropdownMenu
 import requests
@@ -19,6 +19,9 @@ class RegistarScreen(Screen):
         self.ids.telemovel_label.text = ""
         self.ids.clickable_date.ids.data_nascimento_label.text = ""
         self.ids.drop_item.text = ""
+
+        self.reset_field('email_label', 'user@gmail.com')
+        self.reset_field('username_label', 'Escolha um nome de usuário')
 
     def menu_list(self):
         self.menu_items = [
@@ -58,6 +61,17 @@ class RegistarScreen(Screen):
         self.menu.dismiss()
 
     def register_user(self):
+        if not self.validar_nome() or \
+                not self.validar_apelido() or \
+                not self.validar_username() or \
+                not self.validar_email() or \
+                not self.validar_password() or \
+                not self.validar_password2() or \
+                not self.validar_telemovel() or \
+                not self.validar_data_nascimento() or \
+                not self.validar_escola():
+            return
+
         nome = self.ids.nome_label.text
         apelido = self.ids.apelido_label.text
         username = self.ids.username_label.text
@@ -77,19 +91,18 @@ class RegistarScreen(Screen):
         elif escola == "Não":
             escola = "nao"
 
+        # campos_obrigatorios = [nome, apelido, username, email, password,
+        #                        password2, telemovel, data_nascimento, escola]
+
+        # if any(not campo for campo in campos_obrigatorios):
+        #     Snackbar(
+        #         text="Por favor, preencha todos os campos obrigatórios."
+        #     ).open()
+        #     return
+
         if password != password2:
             Snackbar(text="As senhas não coincidem.").open()
             return
-
-        print('Nome: ', nome)
-        print('Apelido: ', apelido)
-        print('Username: ', username)
-        print('Email: ', email)
-        print('Password: ', password)
-        print('Password2: ', password2)
-        print('Telemovel: ', telemovel)
-        print('Data de Nascimento: ', data_nascimento)
-        print('Escola: ', escola)
 
         data = {
             "username": username,
@@ -109,22 +122,128 @@ class RegistarScreen(Screen):
         response = requests.post(
             'http://127.0.0.1:8000/users/api/v1/', json=data)
 
-        print('Response: ', response)
+        # print('Response: ', response)
         print(json.dumps(response.json(), indent=4))
 
-        if response:
+        # if response:
+        print('Status Code: ', response.status_code)
 
-            if response.status_code == 201:
-                print('Utilizador registado com sucesso')
-                self.manager.current = 'screen_inicio'
+        if response.status_code != 201:
+            print('Status code diferente de 201')
 
-            else:
-                try:
-                    error_message = response.json()
-                    print('Erro ao registar utilizador(error_message): ',
-                          error_message)
-                except ValueError:
-                    print('Erro ao registar utilizador(text): ', response.text)
+            try:
+                print('TRY')
+                error_messages = response.json()
+                for field, messages in error_messages.items():
+                    if field == "username":
+                        self.ids.username_label.error = True
+                        self.ids.username_label.helper_text = messages[0]
+                    elif field == "email":
+                        self.ids.email_label.error = True
+                        self.ids.email_label.helper_text = messages[0]
+                    elif field == "password":
+                        self.ids.password_label.error = True
+                        self.ids.password_label.helper_text = messages[0]
+                    # Adicione condições semelhantes para outros campos
+
+            except ValueError:
+                print('Erro ao registar utilizador(text): ', response.text)
+                Snackbar(text=response.text).open()
+            return
+
+        print('Utilizador registado com sucesso')
+        self.manager.current = 'screen_inicio'
+
+    def validar_nome(self):
+        nome = self.ids.nome_label.text
+        if not nome:
+            self.ids.nome_label.error = True  # Marca o campo como com erro
+            Snackbar(text="O campo 'Nome' é obrigatório.").open()
+            return False
+        self.ids.nome_label.error = False  # Remove a marcação de erro
+        return True
+
+    def validar_apelido(self):
+        apelido = self.ids.apelido_label.text
+        if not apelido:
+            self.ids.apelido_label.error = True  # Marca o campo como com erro
+            Snackbar(text="O campo 'Apelido' é obrigatório.").open()
+            return False
+        self.ids.apelido_label.error = False
+        return True
+
+    def validar_username(self):
+        username = self.ids.username_label.text
+        if not username:
+            self.ids.username_label.error = True
+            Snackbar(text="O campo 'Username' é obrigatório.").open()
+            return False
+        self.ids.username_label.error = False
+        return True
+
+    def validar_email(self):
+        email = self.ids.email_label.text
+        if not email:
+            self.ids.email_label.error = True
+            Snackbar(text="O campo 'Email' é obrigatório.").open()
+            return False
+        self.ids.email_label.error = False
+        return True
+
+    def validar_password(self):
+        password = self.ids.password_round.ids.password_label.text
+        if not password:
+            self.ids.password_round.ids.password_label.error = True
+            Snackbar(text="O campo 'Senha' é obrigatório.").open()
+            return False
+        self.ids.password_round.ids.password_label.error = False
+        return True
+
+    def validar_password2(self):
+        password2 = self.ids.password2_round.ids.password2_label.text
+        if not password2:
+            self.ids.password2_round.ids.password2_label.error = True
+            Snackbar(text="O campo 'Confirmar Senha' é obrigatório.").open()
+            return False
+        self.ids.password2_round.ids.password2_label.error = False
+        return True
+
+    def validar_telemovel(self):
+        telemovel = self.ids.telemovel_label.text
+        if not telemovel:
+            self.ids.telemovel_label.error = True
+            Snackbar(text="O campo 'Telemóvel' é obrigatório.").open()
+            return False
+        self.ids.telemovel_label.error = False
+        return True
+
+    def validar_data_nascimento(self):
+        data_nascimento = self.ids.clickable_date.ids.data_nascimento_label.text
+        if not data_nascimento:
+            self.ids.clickable_date.ids.data_nascimento_label.error = True
+            Snackbar(text="O campo 'Data de Nascimento' é obrigatório.").open()
+            return False
+        self.ids.clickable_date.ids.data_nascimento_label.error = False
+        return True
+
+    def validar_escola(self):
+        escola = self.ids.drop_item.text
+        if not escola:
+            self.ids.drop_item.error = True
+            Snackbar(text="O campo 'Estudante' é obrigatório.").open()
+            return False
+        self.ids.drop_item.error = False
+        return True
+
+    def reset_field(self, field_id, default_helper_text):
+        field = self.ids.get(field_id)
+        if field:
+            field.error = False
+            field.helper_text = default_helper_text
+
+
+class MDTextFieldError(BooleanProperty):
+    error = BooleanProperty(False)
 
 
 class ClickableTextFieldRound(MDRelativeLayout):
